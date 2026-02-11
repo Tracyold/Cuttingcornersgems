f5b1975c8de9906f3746041f70e2fcd1e29d9ed3
Tracyold <tracy.old.gold@gmail.com>
2026-02-11 13:21:44 -0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     darkMode: ["class"],
     content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/assets/fonts{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
   theme: {
